518ee33692ebd2a15fbaa5d4eba7d0357365d547
Jedidiah <jedidiah.vaskis@gmail.com>
2022-09-23 12:14:42 -0600

    Add playRound function, tested and verified.

--- Rock Paper Scissors.js
+++ Rock Paper Scissors.js
@@ -1,12 +1,43 @@
 function getComputerChoice() {
     num = Math.floor(Math.random()*100);
     if (num < 33) {
-        return 'Rock';
+        return 'rock';
     }
     else if (num > 66) {
-        return 'Scissors';
+        return 'scissors';
     }
     else {
-        return 'Paper';
+        return 'paper';
     }
 }
+
+function playRound (playerSelection, computerSelection) {
+    if ((playerSelection.toLowerCase() == 'rock' &&
+        computerSelection == 'scissors') ||
+        (playerSelection.toLowerCase() == 'scissors' &&
+        computerSelection == 'paper') ||
+        (playerSelection.toLowerCase() == 'paper' &&
+        computerSelection == 'rock')) {
+            return 'You Win! ' + playerSelection + ' beats ' + computerSelection;
+        }
+    else if ((playerSelection.toLowerCase() == 'paper' &&
+        computerSelection == 'scissors') ||
+        (playerSelection.toLowerCase() == 'rock' &&
+        computerSelection == 'paper') ||
+        (playerSelection.toLowerCase() == 'scissors' &&
+        computerSelection == 'rock')) {
+            return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
+            }
+    else if (playerSelection.toLowerCase() == computerSelection) {
+        return "It's a Tie! Rematch! Rematch! Rematch!"
+    }
+    else {
+        return "Something's wrong..."
+    }
+}
+
+const playerSelection = "Scissors";
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
